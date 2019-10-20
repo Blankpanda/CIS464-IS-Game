4f590ca1b440f2cca941c5a956ac6cdcb8978976
adamdacosta97 <56278548+adamdacosta97@users.noreply.github.com>
2019-10-20 15:47:56 -0400

    Point and Click
    Macrophage that move towards where the mouse is clicked

---
+++ IS_Game/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour {
+
+    public float speed;
+    public bool move;
+    private Vector3 target;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            if(move == false)
+            {
+                move = true;
+            }
+        }
+        if(move == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+	}
+}
