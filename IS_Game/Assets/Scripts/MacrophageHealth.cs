bf45738794975ae0d140f708eae023dd2d8f8e3e
adamdacosta97 <56278548+adamdacosta97@users.noreply.github.com>
2019-10-22 21:20:43 -0400

    Macrophage Health
    Viruses can kill macrophages now after 100 hits

---
+++ IS_Game/Assets/Scripts/MacrophageHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MacrophageHealth : MonoBehaviour
+{
+    public GameObject enemy;
+    public int health = 100;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(health == 0)
+        {
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.tag == "Virus")
+        {
+            Destroy(collision.gameObject);
+            health -= 1;
+        }
+        Debug.Log("WeCollided");
+    }
+}
