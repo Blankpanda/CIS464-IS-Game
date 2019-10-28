778443d70363161ffdbe273f00a86435731d8af7
adamdacosta97 <56278548+adamdacosta97@users.noreply.github.com>
2019-10-28 15:29:21 -0400

    Not Crashing
    Latest update

--- IS_Game/Assets/Scripts/Virus Scripts/AddToHealthyNodeList.cs
+++ IS_Game/Assets/Scripts/Virus Scripts/AddToHealthyNodeList.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class AddToHealthyNodeList : MonoBehaviour
 {
+    //node attribuest
     public int health = 3;
     private bool isInfected = false;
     public Color infectedColor;
+    public Color healthyColor;
+
+    //add to targtet list of potentially infectable nodes
     void Start()
     {
         InfectingVirusBehavior.targetList.Add(this.transform);
     }
 
+    //if infected by a virus, become an infected node.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "InfectingVirus")
@@ -19,6 +24,9 @@ public class AddToHealthyNodeList : MonoBehaviour
             if(health == 0)
             {
                 InfectingVirusBehavior.targetList.Remove(this.transform);
+                GetComponent<SpriteRenderer>().color = infectedColor;
+                GetComponent<VirusSpawner>().enabled = true;
+                GetComponent<AddToHealthyNodeList>().enabled = false;
                 isInfected = true;
             }
             else
@@ -26,17 +34,19 @@ public class AddToHealthyNodeList : MonoBehaviour
                 Destroy(collision.gameObject);
                 health--;
             }
-            
+        }
+        if(collision.transform.tag == "PlayerUnit" && isInfected)
+        {
+            Destroy(collision.gameObject);
+            InfectingVirusBehavior.targetList.Add(this.transform);
+            GetComponent<SpriteRenderer>().color = healthyColor;
+            GetComponent<VirusSpawner>().enabled = false;
+            GetComponent<AddToHealthyNodeList>().enabled = true;
         }
     }
 
+    //become infected or healthy
     void Update()
     {
-        if(isInfected)
-        {
-            GetComponent<SpriteRenderer>().color = infectedColor;
-            GetComponent<VirusSpawner>().enabled = true;
-            GetComponent<AddToHealthyNodeList>().enabled = false;
-        }
     }
 }
